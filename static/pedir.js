8b1d669ca75ebcdb1bbd071aeed82129d7ee14ee
Sandra <lissetdovah@gmail.com>
2020-11-27 21:51:48 -0600

    Petición http Express

---
+++ static/pedir.js
@@ -0,0 +1,13 @@
+const http = new XMLHttpRequest();
+const get = document.querySelector('#get');
+const response = document.querySelector('#response');
+
+get.addEventListener('click', () => {   
+  http.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      response.innerHTML = http.responseText;
+    }
+  }
+  http.open('GET', '/static/otro.html', true);
+  http.send();
+});
